218f7a6fe59854a7d6e5ee297de3ffb1d5149c34
Fahd011 <fahdaleem256@gmail.com>
2025-08-10 16:13:20 +0500

    Fixed uploading bills to supabase functionality

--- backend/app.py
+++ backend/app.py
@@ -35,4 +35,4 @@ signal.signal(signal.SIGTERM, cleanup_on_exit)
 cleanup_orphaned_processes()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)     # use_reloader=False to prevent agent from restarting the server when job is run
