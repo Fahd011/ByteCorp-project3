0eb594bf245b1edc7403ad76c1e47e77066e203a
MalikOwais123 <malikowaisjavaid@gmail.com>
2025-08-27 12:04:09 +0500

    fixing req and update time for testing cron job

--- backend/main.py
+++ backend/main.py
@@ -126,8 +126,8 @@ scheduler = AsyncIOScheduler()
 # # ⏰ For production: run daily at 10 AM
 scheduler.add_job(
     daily_agent_job,
-    # CronTrigger(hour=5, minute=25),
-    CronTrigger(hour=11, minute=53),
+    CronTrigger(hour=7, minute=10),
+    # CronTrigger(hour=11, minute=53),
     id="daily_agent_job",
     replace_existing=True,
 )
