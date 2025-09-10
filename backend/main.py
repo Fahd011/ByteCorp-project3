ffc3e033fac21ea40e5fa2dd9aef4c2dadf9458e
MalikOwais123 <malikowaisjavaid@gmail.com>
2025-09-10 11:56:29 +0500

    update time

--- backend/main.py
+++ backend/main.py
@@ -126,7 +126,7 @@ scheduler = AsyncIOScheduler()
 
 scheduler.add_job(
     daily_agent_job,                     # the function to run
-    CronTrigger(hour=5, minute=59),      # schedule: every day at 19:5 (7:05PM)
+    CronTrigger(hour=6, minute=59),      # schedule: every day at 19:5 (7:05PM)
     id="daily_agent_job",                # unique job identifier
     replace_existing=True                # replace existing job with same ID if already scheduled
 )
